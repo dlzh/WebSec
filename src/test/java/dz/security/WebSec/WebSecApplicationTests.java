8e7e5bbff8b58c6cea9d49219294ab30d7398fe2
dlzh <2853123+dlzh@users.noreply.github.com>
2024-05-26 12:23:50 +0930

    Added files

---
+++ src/test/java/dz/security/WebSec/WebSecApplicationTests.java
@@ -0,0 +1,13 @@
+package dz.security.WebSec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
