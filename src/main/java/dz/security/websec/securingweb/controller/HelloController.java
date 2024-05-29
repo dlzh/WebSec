f8ae04da50a7e9e6ebda215c4272b0a1733d1554
dlzh <2853123+dlzh@users.noreply.github.com>
2024-05-30 01:13:48 +0930

    add a new controller class

---
+++ src/main/java/dz/security/websec/securingweb/controller/HelloController.java
@@ -0,0 +1,15 @@
+package dz.security.websec.securingweb.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.Map;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public ResponseEntity<Map<String, String>> test(){
+       return ResponseEntity.ok().body(Map.of("Hello", "Hello Test Data"));
+    }
+}
